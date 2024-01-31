0eeb34eb5d676d82356b08621fa771182a2a411f
williford abunas <abunaswilliford.com>
2024-02-01 01:42:38 +1300

    working with localstorage

--- src/index.js
+++ src/index.js
@@ -1,82 +1,89 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import {
-  getDatabase,
-  ref,
-  push,
-  onValue,
-  remove,
-} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
-
-const appSettings = {
-  databaseURL:
-    'https://will-s-realtime-database-default-rtdb.asia-southeast1.firebasedatabase.app/',
-}
-
-//set-up DB
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const itemListinDB = ref(database, 'itemList')
-
 const inputEl = document.getElementById('input-el')
 const buttonEl = document.getElementById('button-el')
 const ulEl = document.getElementById('ul-el')
+let itemsArray = []
 
 buttonEl.addEventListener('click', function () {
   let inputValue = inputEl.value
-  push(itemListinDB, inputValue)
+  setToMyListStorage(inputValue)
   clearInput()
+  // appendItemToUl(itemsInStorage)
 })
 
-//fetch itemListinDB
-onValue(itemListinDB, function (snapshot) {
-  if (snapshot.exists()) {
-    let itemsArray = Object.entries(snapshot.val())
+function setToMyListStorage(val) {
+  itemsArray.push(val)
+  localStorage.setItem('myList', JSON.stringify(itemsArray))
 
-    clearShoppingList()
-
-    for (let i = 0; i < itemsArray.length; i++) {
-      let currentItem = itemsArray[i]
-      let currentItemID = currentItem[0]
-      let currentItemValue = currentItem[1]
-
-      appendItemToList(currentItem)
-    }
-  } else {
-    ulEl.innerHTML = 'No items here... yet'
+  if (itemsArray[0]) {
+    let itemsInStorage = JSON.parse(localStorage.getItem('myList'))
+    console.log(itemsInStorage)
+    // appendItemToUl(itemsInStorage)
   }
-})
+}
 
 function clearInput() {
   inputEl.value = ''
 }
 
-function clearShoppingList() {
+function appendItemToUl(arr) {
+  let newEl = document.createElement('li')
+
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i]
+    console.log(item)
+  }
+}
+
+function clearUlEl() {
   ulEl.innerHTML = ''
 }
 
-function appendItemToList(item) {
-  let itemID = item[0]
-  let itemValue = item[1]
+// //fetch itemListinDB
+// onValue(itemListinDB, function (snapshot) {
+//   if (snapshot.exists()) {
+//     let itemsArray = Object.entries(snapshot.val())
 
-  let newLi = document.createElement('li')
-  newLi.classList.add(
-    'bg-[#DCE1EB]',
-    'text-[#432000]',
-    'p-2',
-    'rounded-md',
-    'shadow-md',
-    'text-sm',
-    'grow',
-    'text-center'
-  )
+//     clearShoppingList()
 
-  newLi.textContent = itemValue
+//     for (let i = 0; i < itemsArray.length; i++) {
+//       let currentItem = itemsArray[i]
+//       let currentItemID = currentItem[0]
+//       let currentItemValue = currentItem[1]
 
-  newLi.addEventListener('click', function () {
-    let locationOfItemInDB = ref(database, `itemList/${itemID}`)
+//       appendItemToList(currentItem)
+//     }
+//   } else {
+//     ulEl.innerHTML = 'No items here... yet'
+//   }
+// })
 
-    remove(locationOfItemInDB)
-  })
+// function clearShoppingList() {
+//   ulEl.innerHTML = ''
+// }
 
-  ulEl.append(newLi)
-}
+// function appendItemToList(item) {
+//   let itemID = item[0]
+//   let itemValue = item[1]
+
+//   let newLi = document.createElement('li')
+//   newLi.classList.add(
+//     'bg-[#DCE1EB]',
+//     'text-[#432000]',
+//     'p-2',
+//     'rounded-md',
+//     'shadow-md',
+//     'text-sm',
+//     'grow',
+//     'text-center'
+//   )
+
+//   newLi.textContent = itemValue
+
+//   newLi.addEventListener('click', function () {
+//     let locationOfItemInDB = ref(database, `itemList/${itemID}`)
+
+//     remove(locationOfItemInDB)
+//   })
+
+//   ulEl.append(newLi)
+// }
